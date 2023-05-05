ba00cc628d95e8a8bceb683ff2699ced94deebb7
rokasj <rokas@jerio.me>
2023-05-05 17:36:38 -0400

    Add images folder, add flashing grass png

--- src/game-logic/render.js
+++ src/game-logic/render.js
@@ -17,21 +17,33 @@ export default class Render {
         this.ctx.clearRect(-originCoordX, -originCoordY, this.canvas.width, this.canvas.height);
     }
 
+    drawBackground(map) {
+        const background = new Image();
+        background.src = map.background.src;
+        background.dataset.posX = map.background.absolutepositionx;
+        background.dataset.posY = map.background.absolutepositiony;
+        this.background = background;
+        this.background.onload = () => {
+            this.ctx.drawImage(this.background, this.background.dataset.posX, this.background.dataset.posY)
+        }
+    }
+
     drawMap(map) {
-        this.clearMap()
+        this.clearMap();
+        // this.drawBackground(map); //Uncomment this line to see a flashing grass png
         for(const entityId in map.entities) {
-            const entityProperties = map.entities[entityId]
-            let posX, posY, dimX, dimY
+            const entityProperties = map.entities[entityId];
+            let posX, posY, dimX, dimY;
             [posX, posY, dimX, dimY] = [
                 entityProperties.absolutePosition[0],
                 entityProperties.absolutePosition[1],
                 entityProperties.dimension[0],
                 entityProperties.dimension[1],
-            ]
+            ];
             if(entityId !== "player") {
                 this.ctx.fillStyle = entityProperties.color;
                 this.ctx.fillRect(posX, posY, dimX, dimY);
-            }
+            };
 
             this.ctx.fillStyle = entityProperties.color; //Making sure the player renders last
             this.ctx.fillRect(posX, posY, dimX, dimY);
